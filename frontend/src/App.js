2073bd750fbc2769333509164b5e300b8f85c34f
uvsq22006171 <71390007+uvsq22006171@users.noreply.github.com>
2024-03-06 17:40:09 +0100

    ajour des composant

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,59 @@
-//import logo from './logo.svg';
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
-//import {TestComponent} from './TestComponent';
-import {TestComponent2} from './TestComponent2';
+// import React from "react";
+// //import "./style.css/recipeCard.css";
+// import RecipeCard from "./recipeCard.js";
 
-function App() {
-  const [name, setName] = useState('');
-  useEffect(() => {
-    setName('Hedi');
-  }, []);
+// class App extends React.Component {
+//   render() {
+//     const title = "Coquiles Saint-jacques";
+//     const description = `Ces délicieuses coquilles sont servies de nuit et sont préparées avec des champignons dans une sauce à base de crème fraîche.
+//     Le tout est ensuite gratiné au four pour obtenir une croûte raffinée et savoureuse, une entrée idéale et joliment épaisse!`;
+//     return (
+//       <div className="App">
+//         <RecipeCard title={title} description={description} />
+//       </div>
+//     );
+//   }
+// }
 
-  useEffect(() => {
-    console.log(name);
-  }, [name]);
-  
-  
+// export default App;
+
+//App.js
+import React from "react";
+import "./style.css/ButtonComponent.css";
+import ButtonComponent from "./Components.js/ButtonComponent";
+import "./App.css";
+
+const App = () => {
+  const handleButtonClick = () => {
+    console.log("Bouton cliqué !");
+  };
 
   return (
-    <div className="App">
-      <TestComponent2 name={'Hedi'} onChange={(e) => console.log('My name is', e)} />
-
+    <div className="btn">
+      <div className="button_container_1">
+        <ButtonComponent
+          type="primary"
+          text="Cliquez-moi !"
+          onClick={handleButtonClick}
+        />
+      </div>
+      <div className="button_container_2">
+        <ButtonComponent
+          type="secondary"
+          text="Cliquez-moi aussi !"
+          onClick={handleButtonClick}
+        />
+      </div>
+      <div className="button_container_3">
+        <ButtonComponent
+          type="tertiary"
+          text="Et moi aussi !"
+          onClick={handleButtonClick}
+          className="underline_text"
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
