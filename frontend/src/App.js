fc045ccbe56e4059eb3c8e68fd0e73ef9cc75f7b
uvsq21916099 <thomas.joly@ens.uvsq.fr>
2024-03-06 21:53:00 +0100

    checkboxes work !

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { TextInputComp, getCred } from './TextInput';
+import { CheckBoxComp } from './CheckBox';
 
 function App() {
   return (  
     <div className="App">
       <div className = "userinput">
         <TextInputComp placeholder="type your email here" pass={false}/>
-        <TextInputComp id = "userpass" placeholder="type your password here" pass={true}/>
+        <TextInputComp placeholder="type your password here" pass={true}/>
         <button onClick={getCred}>Connection</button>
+      </div>
+      <div className = "checkbox">
+        <CheckBoxComp labels={["check1","check2"]}/>
       </div>
     </div>
   );
