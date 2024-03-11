9ae3ae0d57c3a4291d018161d7a94239c927af08
uvsq22006171 <71390007+uvsq22006171@users.noreply.github.com>
2024-03-11 11:11:44 +0100

    ajout des composant CommentCrad et CommentCards2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,44 @@
 //import logo from './logo.svg';
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 //import {TestComponent} from './TestComponent';
-import {TestComponent2} from './TestComponent2';
-import ListItem from './components/ListItem';
-import DropDownList from './components/DropDownList';
+import { TestComponent2 } from "./TestComponent2";
+import ListItem from "./components/ListItem";
+import DropDownList from "./components/DropDownList";
 import Tabs from "./components/Tabs";
-import ScreenTemplate from './screens/ScreenTemplate';
+import ScreenTemplate from "./screens/ScreenTemplate";
 
 function App() {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
+
   useEffect(() => {
-    setName('Hedi');
+    setName("Hedi");
   }, []);
 
   useEffect(() => {
     console.log(name);
   }, [name]);
 
-
-
   return (
     <div className="App">
-      {/*
-      <TestComponent2 name={'Hedi'} onChange={(e) => console.log('My name is', e)} />
-      <div style={{width: '50%', marginTop: 16, display: 'flex', alignItems: 'center'}}>
-        <DropDownList onSelect={(t, i) => console.log(t, i)}/>
+      {/* <TestComponent2
+        name={"Hedi"}
+        onChange={(e) => console.log("My name is", e)}
+      /> */}
+      <div
+        style={{
+          width: "50%",
+          marginTop: 16,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        {/* <DropDownList onSelect={(t, i) => console.log(t, i)} /> */}
       </div>
-      /}
       <Tabs>
         <div label="Chercher">
-          {/
           Welcome to the tab, <em>Chercher</em>!
-          */}
-          <ScreenTemplate/>
+          <ScreenTemplate />
         </div>
         <div label="Recettes">
           After a while, <em>Recettes</em>!
--- frontend/src/screens/ScreenTemplate.js
+++ frontend/src/screens/ScreenTemplate.js
@@ -1,14 +1,21 @@
-import * as React from 'react';
-import { useEffect, useState, useContext } from 'react';
+import * as React from "react";
+import { useEffect, useState, useContext } from "react";
 
-import C from '../constants/colors';
-import { APIuri } from '../constants/Api';
+import C from "../constants/colors";
+import { APIuri } from "../constants/Api";
 //import { AuthContext } from '../constants/Context'; Context to be created
 
 /*
 Import your components and constants here
 */
-import DropDownList from '../components/DropDownList';
+import DropDownList from "../components/DropDownList";
+import ButtonComponent from "../components/ButtonComponent";
+import CommentCard from "../components/CommentCards";
+import RecipeCard from "../components/recipeCard";
+import CommentCard2 from "../components/CommentCard2";
+
+import "../style.css/ButtonComponent.css";
+import "../style.css/CommentCards.css";
 
 /*
 Import your used redux here
@@ -17,7 +24,6 @@ Import your used redux here
 //---------------------------------------------------------------------
 
 function ScreenTemplate() {
-
   // put here your constants
 
   const testConstant = "I am a constant";
@@ -27,9 +33,13 @@ function ScreenTemplate() {
 
   const [testState, setTestState] = useState("I am a state variable");
 
+  // const title = "Coquiles Saint-jacques";
+  // const description = `Ces délicieuses coquilles sont servies de nuit et sont préparées avec des champignons dans une sauce à base de crème fraîche.
+  //  Le tout est ensuite gratiné au four pour obtenir une croûte raffinée et savoureuse, une entrée idéale et joliment épaisse!`;
+
   // put here your functions and handlers
 
-  const testFunction = async() => {
+  const testFunction = async () => {
     console.log(`My current state variable value is : ${testState}`);
   };
 
@@ -37,18 +47,40 @@ function ScreenTemplate() {
 
   useEffect(() => {
     testFunction();
-  },[testState]);
+  }, [testState]);
 
   //---------------------------------------------------------
   // Render your screen here
   return (
-    <div className='screen-view-1' style={{backgroundColor: C.white}}>
-      <div className='screen-view-1' style={{ width: '95%', backgroundColor: C.white }}>
+    <div className="screen-view-1" style={{ backgroundColor: C.white }}>
+      <div
+        className="screen-view-1"
+        style={{ width: "95%", backgroundColor: C.white }}
+      >
         {/* put your content here */}
 
-        <DropDownList/>
-
+        <DropDownList />
+        {/* <ButtonComponent type="primary" text="Click me" onClick={testFunction}/> */}
       </div>
+
+      <div className="Comment_Flexbox">
+        <CommentCard />
+      </div>
+
+      <div className="Comment_Flexbox2">
+        <CommentCard2
+          name="Name"
+          date={new Date()}
+          starRating={4}
+          comment="C'est une très bonne recette.Sa seule 
+          erreur est la quantité de sel."
+        />
+      </div>
+
+      {/* <div className="app">
+         <RecipeCard title={title} description={description} />
+        {" "}
+      </div> */}
     </div>
   );
 }
