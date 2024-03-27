67983c7fe3c3f622bad473662724190fca305e19
uvsq22006171 <71390007+uvsq22006171@users.noreply.github.com>
2024-03-27 14:56:10 +0100

    Ajout du dernier screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import './App.css';
-import './styles/ButtonComponent.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Tabs from "./components/Tabs";
 import ScreenTemplate from "./screens/ScreenTemplate";
-import RecipeScreen1 from './screens/RecipeScreen1';
-import RecipeScreen2 from './screens/RecipeScreen2';
+import RecipeScreen1 from "./screens/RecipeScreen1";
+import RecipeScreen2 from "./screens/RecipeScreen2";
+import RecipeScreen3 from "./screens/RecipeScreen3"; // Assurez-vous d'importer le nouveau composant d'écran ici
 
 function App() {
   return (
     <div className="App">
-      <Tabs>
-        <div label="Chercher">
-          {/*
-          Welcome to the tab, <em>Chercher</em>!
-          */}
-          <ScreenTemplate/>
-        </div>
-        <div label="Recettes">
-          <RecipeScreen1/>
-          {/* <RecipeScreen2/> */}
-        </div>
-        <div label="Profil" icon="person-circle">
-        </div>
-      </Tabs>
+      <Router>
+        <Tabs>
+          <div label="Chercher">
+            <ScreenTemplate />
+          </div>
+          <div label="Recettes">
+            <Routes>
+              <Route path="/" element={<RecipeScreen1 />} />
+              <Route
+                path="/detail/:category/:buttonText"
+                element={<RecipeScreen2 />}
+              />
+              <Route
+                path="/detail/:category/:buttonText/recipe/:title2"
+                element={<RecipeScreen3 />}
+              />
+            </Routes>
+          </div>
+          <div label="Profil" icon="person-circle"></div>
+        </Tabs>
+      </Router>
     </div>
   );
 }
--- frontend/src/screens/RecipeScreen1.js
+++ frontend/src/screens/RecipeScreen1.js
@@ -3,149 +3,163 @@ import ImageComponent from "../components/ImageComponent";
 import "../styles/ImageComponent.css";
 import ButtonComponent from "../components/ButtonComponent";
 import "../styles/ButtonRecipe.css";
+import { useNavigate } from "react-router-dom";
 
 function RecipeScreen1() {
+  const navigate = useNavigate();
+
   const handleClick = (category, buttonText) => {
-    console.log(category, ">", buttonText);
+    navigate(`/detail/${category}/${buttonText}`);
   };
   return (
     <div className="recipe-screen">
       <ImageComponent />
-      <div className="button-catégorie">
-        <h3>CATEGORIES</h3>
-        <div className="button1Categorie">
+      <div className="button-catégorie" style={{ textAlign: "center" }}>
+        <h3 style={{ textDecoration: "underline" }}>CATEGORIES</h3>
+        <div>
           <ButtonComponent
             type="primary"
             text="Entrée"
-            onClick={() => handleClick("CATEGORIES","Entrée")}
+            onClick={() => handleClick("CATEGORIES", "Entrée")}
             fontSize={"15px"}
           />
         </div>
-        <div className="button2Categorie">
+        <div>
           <ButtonComponent
             type="primary"
             text="Plats"
-            onClick={() => handleClick("Plats")}
+            onClick={() => handleClick("CATEGORIES", "Plats")}
             fontSize={"15px"}
           />
         </div>
-        <div className="button3Categorie">
+        <div>
           <ButtonComponent
             type="primary"
             text="Desserts"
-            onClick={() => handleClick("Desserts")}
+            onClick={() => handleClick("CATEGORIES", "Desserts")}
             fontSize={"15px"}
           />
         </div>
       </div>
       <div class="rectangle"></div>
-      <div className="button-ingrédient">
-        <h3>INGREDIENT</h3>
-        <div className="button1Ingrédient">
+      <div className="button-ingrédient" style={{ textAlign: "center" }}>
+        <h3 style={{ textDecoration: "underline" }}>INGREDIENT</h3>
+        <div>
           <ButtonComponent
             type="primary"
             text="Viandes"
-            onClick={() => handleClick("Entrée")}
+            onClick={() => handleClick("INGREDIENT", "Viandes")}
             fontSize={"15px"}
           />
         </div>
-        <div className="button2Ingrédient">
+        <div>
           <ButtonComponent
             type="primary"
             text="Poissons"
-            onClick={() => handleClick("Entrée")}
+            onClick={() => handleClick("INGREDIENT", "Poissons")}
             fontSize={"15px"}
           />
         </div>
-        <div className="button3Ingrédient">
+        <div>
           <ButtonComponent
             type="primary"
             text="Fruits"
-            onClick={() => handleClick("Entrée")}
+            onClick={() => handleClick("INGREDIENT", "Fruits")}
             fontSize={"15px"}
           />
         </div>
-        <div className="button4Ingrédient">
+        <div>
           <ButtonComponent
             type="primary"
             text="Légumes"
-            onClick={() => handleClick("Entrée")}
+            onClick={() => handleClick("INGREDIENT", "Légumes")}
             fontSize={"15px"}
           />
         </div>
       </div>
       <div class="rectangle"></div>
-      <div className="button-classique">
-        <h3>LES CLASSIQUES </h3>
-        <div className="button1Classique">
+      <div className="button-classique" style={{ textAlign: "center" }}>
+        <h3 style={{ textDecoration: "underline" }}>LES CLASSIQUES </h3>
+        <div>
           <ButtonComponent
             type="primary"
             text="Recettes pas chères"
-            onClick={() => handleClick("Entrée")}
+            onClick={() => handleClick("LES CLASSIQUES", "Recettes pas chères")}
             fontSize={"15px"}
           />
         </div>
-        <div className="button2Classique">
+        <div>
           <ButtonComponent
             type="primary"
             text="Recette facile et rapide"
-            onClick={() => handleClick("Entrée")}
+            onClick={() =>
+              handleClick("LES CLASSIQUES", "Recette facile et rapide")
+            }
             fontSize={"15px"}
           />
         </div>
-        <div className="button3Classique">
+        <div>
           <ButtonComponent
             type="primary"
             text="Recette anti-gaspillage"
-            onClick={() => handleClick("Entrée")}
+            onClick={() =>
+              handleClick("LES CLASSIQUES", "Recette anti-gaspillage")
+            }
             fontSize={"15px"}
           />
         </div>
       </div>
       <div class="rectangle"></div>
-      <div className="button-fête">
-        <h3> SPECIALE FÊTE</h3>
-        <div className="button1Fête">
+      <div className="button-fête" style={{ textAlign: "center" }}>
+        <h3 style={{ textDecoration: "underline" }}> SPECIALE FÊTE</h3>
+        <div>
           <ButtonComponent
             type="primary"
             text="Ramadan"
-            onClick={() => handleClick("Entrée")}
+            onClick={() => handleClick("SPECIALE FÊTE", "Ramadan")}
             fontSize={"15px"}
           />
         </div>
-        <div className="button2Fête">
+        <div>
           <ButtonComponent
             type="primary"
             text="Noël"
-            onClick={() => handleClick("Entrée")}
+            onClick={() => handleClick("SPECIALE FÊTE", "Noël")}
             fontSize={"15px"}
           />
         </div>
-        <div className="button3Fête">
+        <div>
           <ButtonComponent
             type="primary"
             text="Saint-Valentin"
-            onClick={() => handleClick("Entrée")}
+            onClick={() => handleClick("SPECIALE FÊTE", "Saint-Valentin")}
             fontSize={"15px"}
           />
         </div>
-        <div className="button4Fête">
+        <div>
           <ButtonComponent
             type="primary"
             text="Pâques"
-            onClick={() => handleClick("Entrée")}
+            onClick={() => handleClick("SPECIALE FÊTE", "Pâques")}
             fontSize={"15px"}
           />
         </div>
       </div>
       <div class="rectangle"></div>
-      <div className="button-Favoris">
-        <h3>FAVORIS</h3>
-        <div className="buttonFavoris">
+      <div className="button-Favoris" style={{ textAlign: "center" }}>
+        <h3 style={{ textDecoration: "underline" }}>FAVORIS</h3>
+        <div 
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "150px",
+          }}
+        >
           <ButtonComponent
             type="primary"
             text="Voir mes favoris"
-            onClick={() => handleClick("Entrée")}
+            // onClick={() => handleClick()}
             fontSize={"15px"}
           />
         </div>
