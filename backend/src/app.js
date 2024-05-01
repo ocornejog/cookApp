76e8ebddd16042219a044f484f6c95db5b69940d
Oscar Cornejo <oscar9.cornejo@gmail.com>
2024-05-01 23:08:24 +0200

    Changed app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,9 +23,6 @@ app.use(cors(
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({extended: false}));
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../frontend/build')));
-
 //main root to serve webapp frontend
 app.get('/api', (req, res) => {
     res.send(`Version 0.0.1`);
@@ -38,10 +35,5 @@ app.use('/api/appRecipes', require('./routes/AppRecipesRoute'));
 app.use('/api/comments', require('./routes/CommentsRoute'));
 app.use('/api/favoritesRecipes', require('./routes/FavoritesRecipesRoute'));
 app.use('/api/email', require('./routes/EmailRoute'));
-
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-});
 
 module.exports = app;
