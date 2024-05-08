2e24cd098a5b6141a8bcce5448bf5e30d61e5fc1
uvsq22006171 <71390007+uvsq22006171@users.noreply.github.com>
2024-05-08 11:17:46 +0200

    ajout de background image

--- frontend/src/components/ImageComponent.js
+++ frontend/src/components/ImageComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const image = process.env.PUBLIC_URL + "/backgroud.png";
+const image = process.env.PUBLIC_URL + "/_1407d804-e645-4c0e-b2cb-506ab54844ac.jpeg";
 const ImageComponent = () => {
     return (
         <div className="image-container">
